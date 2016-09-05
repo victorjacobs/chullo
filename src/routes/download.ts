d4955bdb98f3cad1dab2807b753a2b2d50ed120f
Victor Jacobs <victor.jacobs@me.com>
2016-09-05 23:27:31 +0200

    Fix download route not having proper Content-Type and Content-Length set

--- src/routes/download.ts
+++ src/routes/download.ts
@@ -7,6 +7,10 @@ router.get('/:fileId', (req, res) => {
     File.findById(req.params.fileId, (err, file) => {
         if (err) return res.status(400).json(err);
 
+        // No idea why res.download wouldn't set the following headers but okay
+        res.setHeader('Content-Type', file.mime);
+        res.setHeader('Content-Length', file.size.toString());
+
         res.download(file.path, file.name);
     });
 });
